79e4be2bb54fa85d44a773314e266fb220e9f61b
swimme <2sy100@likelion.org>
2018-11-04 17:22:37 +0900

    bootstrap+devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # 수영
+  #devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
