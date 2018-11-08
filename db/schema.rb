e5e8446ffdbc9e8f4b1cab2187e6ca0b42c4026d
PoSungKim <psungkr94@likelion.org>
2018-11-09 00:58:33 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -136,6 +136,11 @@ ActiveRecord::Schema.define(version: 2018_11_07_025910) do
     t.string "ages"
     t.string "profileimg"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
