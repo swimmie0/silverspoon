207031d170e68fa217f9e3079b479f3587578847
jiwonny <whdwhd0115@likelion.org>
2018-11-08 19:07:04 +0900

    search 기본 기능

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  ##SEARCH------resources :zizuminfos 보다 위에
+  get 'zizuminfos/search'
+  ##-------------------------------------
+
+
+  #SEARCH----------resouces :restaurants 보다 위에 가게 해야 함! --------------------
+  get 'restaurants/search' => "restaurants#search"
+  get 'menus/search/' => "menus#search"
+  get 'menus/getMenu' => "menus#getMenu"
+  get 'menus/index' => "menus#index", as: 'menus'
+  #--------------------------------------------------------------------------
+
+
   resources :userrequests
   get 'home/index'
   root 'home#index'
@@ -7,24 +21,14 @@ Rails.application.routes.draw do
   resources :menus
   devise_for :users
 
-  #크롤링
+  #============크롤링===============
   get '/crawling' => 'restaurants#crawling'
+  #================================
+
   # 수영
   #devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
-    ### 수정 / 삭제 요청 ###
-  #요청 확인
-  get '/userrequests' => 'userrequests#index'
-  #메뉴 추가 요청
-  get '/userrequests/new_request' => 'userrequests#new_request'
-  post '/userrequests/create' => 'userrequests#create'
-  #메뉴 수청 / 삭제 요청
-  get '/userrequests/edit_request' => 'userrequests#edit_request'
-  post '/userrequests/edit_request' => 'userrequests#edit_request'
-  #승인
-  post '/userrequests/permit' => 'userrequests#permit'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #============수정요청=============
   ## 수정 / 삭제 요청 ###
   #요청 확인
   get '/userrequests' => 'userrequests#index'
@@ -36,5 +40,7 @@ Rails.application.routes.draw do
   post '/userrequests/edit_request' => 'userrequests#edit_request'
   #승인
   post '/userrequests/permit' => 'userrequests#permit'
+  #=================================
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
