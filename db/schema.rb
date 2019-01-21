b0a75173e5be08b3ed24b3d34a56f36dfdbfafd9
jiwonny <whdwhd0115@likelion.org>
2019-01-22 02:50:46 +0900

    레시피 태그 기능

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_142823) do
+ActiveRecord::Schema.define(version: 2019_01_21_174008) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -180,6 +180,8 @@ ActiveRecord::Schema.define(version: 2019_01_09_142823) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "explain"
+    t.text "ingredients"
+    t.text "tag"
     t.text "content1"
     t.string "recipeimage1"
     t.text "content2"
@@ -202,6 +204,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_142823) do
     t.string "recipeimage10"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "allergyfor"
   end
 
   create_table "restaurants", force: :cascade do |t|
