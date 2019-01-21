b8c50544e2e738c8b43d7d289bc74482ddc45fd7
swimme <2sy100@likelion.org>
2019-01-21 23:18:27 +0900

    알림 수정1/2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,22 +11,33 @@ class CommentsController < ApplicationController
             make_child_comment
             format.html  { redirect_to("#{request.referrer}#comment#{@comment.id}", :notice => '댓글이 작성되었습니다.') }
                 # 댓글에 대댓글 작성 시 댓글 작성자에게 알림이 울림.
-                if @comment.parent != nil && @comment.parent.user != current_user
-                    @new_alarm = NewAlarm.create! user:  @comment.parent.user,
-                                                  # content: "#{current_user.name.truncate(15, omission: '...')} 님이 답댓글을 달았습니다.",
-                                                  content: "",
-                                                  link: request.referrer
+                # if @comment.parent != nil && @comment.parent.user != current_user
+                #     @new_alarm = NewAlarm.create! user:  @comment.parent.user,
+                #                                   # content: "#{current_user.name.truncate(15, omission: '...')} 님이 답댓글을 달았습니다.",
+                #                                   # content: "",
+                #                                   link: request.referrer
+                                                  
                   
-                end  
+                # end  
         else
+
             format.html  { redirect_to(request.referrer, :alert => '댓글 내용을 작성해주세요.') }
         end
       end
-
-      if @comment.parent == nil && commentable.user != current_user
-        @new_alarm = NewAlarm.create! user: commentable.user,
-                                    # content: "#{current_user.name.truncate(15, omission: '...')} 님이 댓글을 달았습니다.",
-                                    link: request.referrer
+## 수정해야할부분 내작성글에 댓글달았을 때 거기에 답글다는경우(작성글댓글로알림)/내작성글에 a유저의 댓글에 b유저가 답글다는경우(나에게는 작성글댓글, a유저에겐 답글알림)
+      if @comment.parent == nil && commentable.user != current_user 
+        # puts "테스트 #{NewAlarm.where(link: freeboard_path(:id=>commentable)).count}끝"
+        @alarms = NewAlarm.where(link: freeboard_path(:id=>commentable))
+        if @alarms.count != 0
+          @alarms[0].mark_as_read! for: commentable.user
+          @new_alarm = NewAlarm.create! user: commentable.user,
+                                          content: "#{Comment.where(commentable: commentable).count}",
+                                          link: URI(request.referer).path
+        else 
+          @new_alarm = NewAlarm.create! user: commentable.user,
+                                        content: "#{Comment.where(commentable: commentable).count}",
+                                        link: URI(request.referer).path
+        end  
       end                             
     end
 
