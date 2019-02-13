78a92d8b3515cd10f527e01f0b1df4aef4b887c4
jiwonny <whdwhd0115@likelion.org>
2019-02-14 02:41:03 +0900

    res/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @popular_recipes = Recipe.order("created_at desc").limit(3);
+    @recent_res = Freeboard.where(category: "정보글").order("created_at desc").limit(25);
+    @recent_adv = Freeboard.where(category: "홍보글").order("created_at desc").limit(25);
+    
+    @res_length = @recent_res.length()
+    @adv_length = @recent_adv.length()
   end
 end
