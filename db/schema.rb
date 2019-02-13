5a0ca707167e348f9de1c29d127d80b50006f47a
PoSungKim <psungkr94@likelion.org>
2019-02-13 21:53:23 +0900

    start

--- db/schema.rb
+++ db/schema.rb
@@ -166,12 +166,6 @@ ActiveRecord::Schema.define(version: 2019_02_02_164455) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.datetime "created_at", null: false
